0403ada99fb250b8234bd8ae1e67a476357175a9
duongaaa101104 <duongaaa101104@gmail.com>
2025-11-06 18:37:53 +0700

    Initial commit - upload food-del backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from "express"
 import cors from "cors"
+import path from "path";
+import { connectDB } from "./config/db.js";
+import foodRoutes from "./routes/foodRoutes.js";
+
 
 // app config
 const app = express()
@@ -8,6 +12,17 @@ const port = 4000
 // middleware
 app.use(express.json())
 app.use(cors())
+
+// DB connection
+connectDB();
+
+// Serve uploaded images (thêm dòng này)
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+//API endpoint
+app.use("/api/food",foodRoutes)
+app.use("/images", express.static('uploads'))
+
 app.get("/", (req, res) => {
     res.send("API Working")
 })
@@ -15,3 +30,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
+//npm run server
